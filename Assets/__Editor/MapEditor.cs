05323516c9d48727d3bb718471d86080d4ea553e
zcloud <zcloud01@outlook.com>
2015-12-27 07:50:21 +0800

    add tile map

---
+++ Assets/__Editor/MapEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapEditor : Editor {
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        MapGenerator generator = target as MapGenerator;
+        generator.GenerateMap();
+    }
+}
---
+++ Assets/__Script/MapGenerator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour {
+    public Transform tilePrefab;
+    public Vector2 mapSize;
+    [Range(0, 1)]
+    public float outlinePercent;
+
+	// Use this for initialization
+	void Start () {
+	    GenerateMap();
+	}
+
+    public void GenerateMap() {
+        const string holderName = "Generated Map";
+        Transform mapHolder = transform.FindChild(holderName);
+        if (mapHolder) {
+            DestroyImmediate(mapHolder.gameObject);
+        }
+
+        mapHolder = new GameObject(holderName).transform;
+        mapHolder.parent = this.transform;
+
+        for (int x = 0; x < mapSize.x; x++) {
+            for (int y = 0; y < mapSize.y; y++) {
+                Vector3 tilePos = new Vector3(-mapSize.x/2+.5f+x, 0, -mapSize.y/2+.5f+y);
+                Transform newTile = Instantiate(tilePrefab, tilePos, Quaternion.Euler(Vector3.right * 90)) as Transform;
+                newTile.localScale = Vector3.one * (1 - outlinePercent);
+                newTile.parent = mapHolder;
+            }
+        }
+    }
+}
