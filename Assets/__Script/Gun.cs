e8ee470db66ce49a53c83e50698966326e2d8d3a
zcloud <zcloud01@outlook.com>
2015-12-29 07:12:36 +0800

    add gun effects

--- Assets/__Script/Gun.cs
+++ Assets/__Script/Gun.cs
@@ -6,13 +6,23 @@ public class Gun : MonoBehaviour {
     public Projectile projectile;
     public float msBetweenShots = 100f;
     public float muzzleVelocity = 35f;
+    public Transform shell;
+    public Transform shellEjection;
     private float nextShotTime;
+    private MuzzleFlash muzzleFlash;
+
+    void Start() {
+        muzzleFlash = GetComponent<MuzzleFlash>();
+    }
 
     public void Shoot() {
         if(Time.time > nextShotTime) {
             nextShotTime = Time.time + msBetweenShots / 1000f;
             Projectile newProjectile = Instantiate(projectile, muzzle.position, muzzle.rotation) as Projectile;
             newProjectile.SetSpeed(muzzleVelocity);
+
+            Instantiate(shell, shellEjection.position, shellEjection.rotation);
+            muzzleFlash.Activate();
         }
     }
 }
---
+++ Assets/__Script/Shell.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shell : MonoBehaviour {
+    public Rigidbody mRigidbody;
+    public float minForce;
+    public float maxForce;
+    private float lifeTime = 4f;
+    private float fadeTime = 2f;
+
+	// Use this for initialization
+	void Start () {
+	    float force = Random.Range(minForce, maxForce);
+        mRigidbody.AddForce(transform.forward * force);
+        mRigidbody.AddTorque(Random.insideUnitSphere * force);
+	    StartCoroutine(Fade());
+	}
+
+    IEnumerator Fade() {
+        yield return new WaitForSeconds(lifeTime);
+
+        float percent = 0;
+        float fadeSpeed = 1/fadeTime;
+        Material mat = GetComponent<Renderer>().material;
+        Color initialColor = mat.color;
+
+        while (percent < 1) {
+            percent += Time.deltaTime*fadeSpeed;
+            mat.color = Color.Lerp(initialColor, Color.clear, percent);
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+}
